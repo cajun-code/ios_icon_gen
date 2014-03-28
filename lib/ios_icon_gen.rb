557c85e1a81ca61fd1703b126f1123e2c0cf02a9
Allan Davis <cajun.code@gmail.com>
2014-03-28 09:07:40 -0400

    Added bin Script

--- lib/ios_icon_gen.rb
+++ lib/ios_icon_gen.rb
@@ -1,5 +1,23 @@
+#! usr/bin/env ruby
 require "ios_icon_gen/version"
+require 'thor'
+require 'rmagick'
 
 module IosIconGen
-  # Your code goes here...
+
+  class IconGen < Thor::Group
+    argument :file_name, :type => :string, :desc => "name of the icon to process"
+
+    def gen_images
+      sizes = [512, 152, 144, 128, 120, 114, 100, 80, 76, 72, 58, 57, 50, 40, 29]
+
+      image = Magick::Image.read(file_name)
+      ext = File.extname(file_name)
+      base = File.basename file_name, ext
+
+      sizes.each{|x| img = image[0].scale(x, x); img.write("#{base}_#{x}#{ext}")}
+    end
+  end
+
 end
+
